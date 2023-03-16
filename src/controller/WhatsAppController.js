6e6c3a51a9f0c4a7df00917316c9cf9bda30a5b2
Ricardo Lucena <ricardo.lucena.clear@gmail.com>
2023-03-16 12:50:27 -0300

    Function to format milliseconds in minute and second

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -178,7 +178,7 @@ elemetsProtoType(){
     startRecordMicrophoneTime(){
         let start = Date.now();
         this._recordMicrophoneInterval = setInterval(()=>{
-            this.el.recordMicrophoneTimer.innerHTML = (Date.now()- start);
+            this.el.recordMicrophoneTimer.innerHTML = Format.toTime(Date.now()- start);
         }, 100);
     }
     closeRecordMicrophone(){
