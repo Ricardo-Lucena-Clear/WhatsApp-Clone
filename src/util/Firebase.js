25618bc30a81e574cc8219a425c56a86d2331b9f
Ricardo Lucena <ricardo.lucena.clear@gmail.com>
2023-03-20 11:09:28 -0300

    Configuring Firebase in the project

---
+++ src/util/Firebase.js
@@ -0,0 +1,32 @@
+const firebase = require ('firebase');
+require ('firebase/firestore')
+
+export class Firebase{
+    constructor(){
+        this._config = {
+            apiKey: "AIzaSyAXwaUJrxIsFdZZKBjK7_D30q-ZlG5TVvg",
+            authDomain: "whatssapp-clone-a35d4.firebaseapp.com",
+            projectId: "whatssapp-clone-a35d4",
+            storageBucket: "whatssapp-clone-a35d4.appspot.com",
+            messagingSenderId: "909022850356",
+            appId: "1:909022850356:web:c492f2156022f525224961",
+            measurementId: "G-GGV2EHD6SP"
+          };
+        this.init();
+    }
+    init(){
+        if (!this._initialized) {
+            firebase.initializeApp(this._config);
+                firebase.firestore().settings({
+                    timestampsInSnapshots: true
+                });
+            this._initialized = true;
+        }
+    }
+    static db(){
+        return firebase.firestore();
+    }
+    static hd(){
+        return firebase.storage();
+    } 
+} 
