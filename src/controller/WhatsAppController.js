34f10f390b91c2e136856825cc8b8f80d6e4cdec
Ricardo Lucena <ricardo.lucena.clear@gmail.com>
2023-03-20 10:25:09 -0300

    Creating event to visualize recording time

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -243,12 +243,14 @@ elemetsProtoType(){
         this.el.btnSendMicrophone.on('click', e=>{ 
             this.el.recordMicrophone.show();
             this.el.btnSendMicrophone.hide();
-            this.startRecordMicrophoneTime();
             this._microphoneController = new MicrophoneController();
             this._microphoneController.on('ready', musica =>{
                 console.log('ready event');
                 this._microphoneController.startRecorder();
             });
+            this._microphoneController.on('recordtimer', timer => {
+                this.el.recordMicrophoneTimer.innerHTML = Format.toTime(timer);
+            })
         });
         this.el.btnCancelMicrophone.on('click', e=>{
             this.closeRecordMicrophone();
@@ -314,16 +316,9 @@ elemetsProtoType(){
             })
         })
     }
-    startRecordMicrophoneTime(){
-        let start = Date.now();
-        this._recordMicrophoneInterval = setInterval(()=>{
-            this.el.recordMicrophoneTimer.innerHTML = Format.toTime(Date.now()- start);
-        }, 100);
-    }
     closeRecordMicrophone(){
         this.el.recordMicrophone.hide();
         this.el.btnSendMicrophone.show();
-        clearInterval(this._recordMicrophoneInterval)
     }
     closeAllMainPanel(){
         this.el.panelMessagesContainer.hide();
