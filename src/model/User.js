951833b361731109fa2c3c2f584bbc45ce3ab19a
Ricardo Lucena <ricardo.lucena.clear@gmail.com>
2023-03-21 10:19:26 -0300

    Listing contacts

--- src/model/User.js
+++ src/model/User.js
@@ -28,7 +28,18 @@ export class User extends Model {
         this.getByKey();
 
     }
+    static getContactsRef(id) {
 
+        return User.getRef()
+        .doc(id)
+        .collection('contacts')        
+
+    }
+
+    static findByEmail(email){
+
+        return User.getRef().doc(email);
+    }
     getByKey(){
 
         return new Promise((s, f)=>{
@@ -86,11 +97,36 @@ export class User extends Model {
     }
     addContact(contact){
 
-        return User.getRef()
-        .doc(this.email)
-        .collection('contacts')
+        return User.getContactsRef(this.email)
         .doc(btoa(contact.email))
         .set(contact.toJSON());
+
+    }
+    getContacts(){
+
+        return new Promise((s, f)=>{
+
+            User.getContactsRef(this.email).onSnapshot(docs => {
+
+                let contacts = [];
+
+                docs.forEach(doc => {
+
+                    let data = doc.data();
+
+                    data.id = doc.id;
+
+                    contacts.push(data);
+
+                });
+
+                this.trigger('contactschange', docs);
+
+                s(contacts);
+
+            });
+
+        });
 
     }
 
