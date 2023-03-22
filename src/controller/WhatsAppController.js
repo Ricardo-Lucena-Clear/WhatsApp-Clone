e9b3459a266fdc03cf6b630b805d391d90004c99
Ricardo Lucena <ricardo.lucena.clear@gmail.com>
2023-03-22 10:08:33 -0300

    Scrolling down automatically if pressed down.

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -121,7 +121,48 @@ export class WhatsAppController {
                     this.el.main.css({
                         display:'flex'
                     });
-                    Message.getRef(this._contactActive.chatId)
+                    this.el.panelMessagesContainer.innerHTML = '';
+                    Message.getRef(this._contactActive.chatId).orderBy('timeStamp')
+        .onSnapshot(docs => {
+
+            
+                    let scrollTop = this.el.panelMessagesContainer.scrollTop;
+                    let scrollTopMax = (this.el.panelMessagesContainer.scrollHeight - this.el.panelMessagesContainer.offsetHeight);
+                    let autoScroll  = (scrollTop >= scrollTopMax);
+
+            docs.forEach(doc => {
+                
+
+                let data = doc.data();
+                data.id = doc.id;
+
+                if (!this.el.panelMessagesContainer.querySelector('#_' + data.id))
+                {
+
+                    let message = new Message();
+
+                    message.fromJSON(data);
+
+                    let me = (data.from === this._user.email);
+
+                    let view = message.getViewElement(me);
+
+                    this.el.panelMessagesContainer.appendChild(view);
+                }
+
+            });
+            if (autoScroll) {
+
+                this.el.panelMessagesContainer.scrollTop = (this.el.panelMessagesContainer.scrollHeight - this.el.panelMessagesContainer.offsetHeight);
+
+            } else {
+
+                this.el.panelMessagesContainer.scrollTop = scrollTop;
+
+            }
+
+        });
+
     }
 initAuth(){
     this._firebase.initAuth().then(response=>{
@@ -148,23 +189,6 @@ initAuth(){
             display: 'flex'
 
        })
-       Message.getRef(this._contactActive.chatId).orderBy('timeStamp')
-       .onSnapshot(docs => {
-           this.el.panelMessagesContainer.innerHTML = '';
-           docs.forEach(doc => {
-                let data = doc.data();
-                data.id = doc.id;
-                if (!this.el.panelMessagesContainer.querySelector('#' + data.id))
-                {
-                let message = new Message();
-                message.fromJSON(data);
-                let me = (data.from === this._user.email);
-                let view = message.getViewElement(me);
-                this.el.panelMessagesContainer.appendChild(view);
-                }
-           });
-       });
-      
        })
     })
     .catch(err=>{
