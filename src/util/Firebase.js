be0633d6cabafdbd7a8b3d8eb2a001b411a125c3
Ricardo Lucena <ricardo.lucena.clear@gmail.com>
2023-03-23 09:04:40 -0300

    Updating last sent message manually

---
+++ src/util/Firebase.js
@@ -0,0 +1,45 @@
+const firebase = require ('firebase');
+require ('firebase/firestore')
+
+export class Firebase{
+    constructor(){
+        this._config = {
+            apiKey: "AIzaSyAXwaUJrxIsFdZZKBjK7_D30q-ZlG5TVvg",
+            authDomain: "whatssapp-clone-a35d4.firebaseapp.com",
+            projectId: "whatssapp-clone-a35d4",
+            storageBucket: "whatssapp-clone-a35d4.appspot.com",
+            messagingSenderId: "909022850356",
+            appId: "1:909022850356:web:c492f2156022f525224961",
+            measurementId: "G-GGV2EHD6SP"
+          };
+        this.init();
+    }
+    init(){
+        if (!window._initializedFirebase) {
+            firebase.initializeApp(this._config);
+                firebase.firestore().settings({
+                    timestampsInSnapshots: true
+                });
+                window._initializedFirebase = true;
+        }
+    }
+    initAuth(){
+
+        return new Promise((resolve, reject)=>{
+            let provider = new firebase.auth.GoogleAuthProvider();
+            firebase.auth().signInWithPopup(provider).then(function (result) {
+                let token = result.credential.accessToken;
+                let user = result.user;
+                resolve(user, token);
+            }).catch(function (error) {
+                reject(error);
+            });
+        });        
+    }
+    static db(){
+        return firebase.firestore();
+    }
+    static hd() {
+        return firebase.storage();
+    }
+}
